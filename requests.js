546a6fc0f9346036e105ab339661d29107e7b614
AvidDabbler <walter.k.jenkins@gmail.com>
2020-04-27 15:03:45 -0500

    update to requests.js

--- requests.js
+++ requests.js
@@ -1,7 +1,7 @@
 // import { agol } from './private.js';
 import { agol } from './personal_agol.js';
 import { requestList, iframe_gen } from './survey.js';
-import { facilities } from './assets/facilities.j'
+import { facilities } from './assets/facilities.js'
 
 (async () => {    
     
